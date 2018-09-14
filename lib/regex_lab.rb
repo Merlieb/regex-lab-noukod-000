e11d2a8a3a9d72ba2593c3cc52e1018a111d0bee
Merline Belizaire <merline@noukod.com>
2018-09-14 19:33:55 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-word.scan(\w)
+  word.match (/^[aeiouAEIOU]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/un[\w]*ing/)
+  text.scan(/un\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-  text.scan(/\b\w{5}\b/)
+  text.scan(/\b[a-z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/^[A-Z].*\W$/)!=[]
+  text.match (/^[A-Z].+\W$/)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/\(\d{3}\)\d{3}-\d{4}|\d{10}|\d{3}\s\d{3}\s\d{3}|\(\d{3}\)\d{7}/)!=[]
+  phone.match (/.?\d{3}.?\d{3}.?\d{4}\b/)
 end
